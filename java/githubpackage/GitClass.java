2291784b4db31251b483c5b365956201fe94cd0f
Rikina Patel <rikinapatel@gmail.com>
2023-10-30 21:29:20 +0000

    filemodified

--- java/githubpackage/GitClass.java
+++ java/githubpackage/GitClass.java
@@ -2,6 +2,6 @@ package githubpackage;
 
 public class GitClass {
     public static void main(String[] args){
-        System.out.println("HELLO GITHUB");
+        System.out.println("HELLO GITHUB RIKINA");
     }
 }
